710a76d8abf9a6e5bbacb5277df4043d5530e1c0
Victor Prieto <victorpg182006@gmail.com>
2025-09-30 18:43:45 +0200

    pr131 Fet + Test

--- RA1/PT1.3/src/main/java/com/project/pr13/PR131Main.java
+++ RA1/PT1.3/src/main/java/com/project/pr13/PR131Main.java
@@ -2,101 +2,99 @@ package com.project.pr13;
 
 import java.io.File;
 
-import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
-/**
- * Classe principal que crea un document XML amb informació de llibres i el guarda en un fitxer.
- * 
- * Aquesta classe permet construir un document XML, afegir elements i guardar-lo en un directori
- * especificat per l'usuari.
- */
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class PR131Main {
 
     private File dataDir;
 
-    /**
-     * Constructor de la classe PR131Main.
-     * 
-     * @param dataDir Directori on es guardaran els fitxers de sortida.
-     */
+    // Constructor que rep el directori base on crear el fitxer
     public PR131Main(File dataDir) {
         this.dataDir = dataDir;
     }
 
-    /**
-     * Retorna el directori de dades actual.
-     * 
-     * @return Directori de dades.
-     */
+    // Getter del directori base
     public File getDataDir() {
         return dataDir;
     }
 
-    /**
-     * Actualitza el directori de dades.
-     * 
-     * @param dataDir Nou directori de dades.
-     */
+    // Setter del directori base
     public void setDataDir(File dataDir) {
         this.dataDir = dataDir;
     }
 
-    /**
-     * Mètode principal que inicia l'execució del programa.
-     * 
-     * @param args Arguments passats a la línia de comandament (no s'utilitzen en aquest programa).
-     */
+    // Mètode per crear i escriure el fitxer XML amb el nom passat
+    public void processarFitxerXML(String nomFitxer) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.newDocument();
+
+            // Crear arrel <biblioteca>
+            Element biblioteca = doc.createElement("biblioteca");
+            doc.appendChild(biblioteca);
+
+            // Crear un llibre amb les dades que demana el test
+            Element llibre = doc.createElement("llibre");
+            llibre.setAttribute("id", "001");
+
+            afegirElement(doc, llibre, "titol", "El viatge dels venturons");
+            afegirElement(doc, llibre, "autor", "Joan Pla");
+            afegirElement(doc, llibre, "anyPublicacio", "1998");
+            afegirElement(doc, llibre, "editorial", "Edicions Mar");
+            afegirElement(doc, llibre, "genere", "Aventura");
+            afegirElement(doc, llibre, "pagines", "320");
+            afegirElement(doc, llibre, "disponible", "true");
+
+            biblioteca.appendChild(llibre);
+
+            // Escriure el document a fitxer dins dataDir
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+
+            // Configurar sortida amb indentació
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+            DOMSource source = new DOMSource(doc);
+            File fitxerSortida = new File(dataDir, nomFitxer);
+            StreamResult result = new StreamResult(fitxerSortida);
+
+            transformer.transform(source, result);
+
+            System.out.println("Fitxer " + nomFitxer + " creat correctament a " + dataDir.getAbsolutePath());
+
+        } catch (ParserConfigurationException | TransformerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Mètode auxiliar per afegir un element fill amb text
+    private void afegirElement(Document doc, Element pare, String nomElement, String text) {
+        Element elem = doc.createElement(nomElement);
+        elem.appendChild(doc.createTextNode(text));
+        pare.appendChild(elem);
+    }
+
+    // Main per proves manuals (opcional)
     public static void main(String[] args) {
-        String userDir = System.getProperty("user.dir");
-        File dataDir = new File(userDir, "data" + File.separator + "pr13");
-
-        PR131Main app = new PR131Main(dataDir);
-        app.processarFitxerXML("biblioteca.xml");
-    }
-
-    /**
-     * Processa el document XML creant-lo, guardant-lo en un fitxer i comprovant el directori de sortida.
-     * 
-     * @param filename Nom del fitxer XML a guardar.
-     */
-    public void processarFitxerXML(String filename) {
-        if (comprovarIDirCrearDirectori(dataDir)) {
-            Document doc = construirDocument();
-            File fitxerSortida = new File(dataDir, filename);
-            guardarDocument(doc, fitxerSortida);
+        try {
+            File directori = new File(".");
+            PR131Main app = new PR131Main(directori);
+            app.processarFitxerXML("biblioteca.xml");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    /**
-     * Comprova si el directori existeix i, si no és així, el crea.
-     * 
-     * @param directori Directori a comprovar o crear.
-     * @return True si el directori ja existeix o s'ha creat amb èxit, false en cas contrari.
-     */
-    private boolean comprovarIDirCrearDirectori(File directori) {
-        if (!directori.exists()) {
-            return directori.mkdirs();
-        }
-        return true;
-    }
-
-    /**
-     * Crea un document XML amb l'estructura d'una biblioteca i afegeix un llibre amb els seus detalls.
-     * 
-     * @return Document XML creat o null en cas d'error.
-     */
-    private static Document construirDocument() {
-        // *************** CODI PRÀCTICA **********************/
-       return null; // Substitueix pel teu
-    }
-
-    /**
-     * Guarda el document XML proporcionat en el fitxer especificat.
-     * 
-     * @param doc Document XML a guardar.
-     * @param fitxerSortida Fitxer de sortida on es guardarà el document.
-     */
-    private static void guardarDocument(Document doc, File fitxerSortida) {
-        // *************** CODI PRÀCTICA **********************/
     }
 }
